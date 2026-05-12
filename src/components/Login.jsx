3f1218f001c65e383ffa13d9019fcde5aa7dbd31
nathalie-jane <nathalie.zheng@gmail.com>
2026-05-12 22:05:15 +0200

    feat(auth-login): add initial login setup
    - Add regex pattern for username and password validation
    - Add initial login component structure and authentication logic for username

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,47 @@
+import { useState } from "react";
+
+/* ----------------------------------------------------
+	VALIDATION PATTERNS
+    - Define RegEx patterns for valid username and
+    password inputs
+------------------------------------------------------- */
+
+const validationPattern = {
+	username: /^[A-Za-z0-9_-]+$/, // Allows only letters, numbers, underscore and hyphen
+	passwordUppercase: /[A-Z]/, // At least one uppercase letter
+	passwordLowercase: /[a-z]/, // At least one lowercase letter
+	passwordNumber: /\d/, // At least one number
+	passwordSpecialCharacter: /[!@#$%^&*?_\-]/, // At least one special character
+};
+
 function Login() {
-  return (
-    <div>
-      <h1>Game-Login</h1>
-    </div>
-  )
+	/* ----------------------------------------------------
+	VALIDATION: USERNAME
+    - Check length (3-20 characters)
+    - Check for valid username characters
+    - Display error message if validation fails
+------------------------------------------------------- */
+
+	function validateUsername(username) {
+		const usernameLength = username.length;
+		const isValidUsername = validationPattern.username.test(username);
+
+		if (usernameLength < 3 || usernameLength > 20) {
+			return "Username must be between 3-20 characters.";
+		}
+
+		if (!isValidUsername) {
+			return "Please enter a valid username.";
+		}
+
+		return "";
+	}
+
+	return (
+		<div>
+			<h1>Game-Login</h1>
+		</div>
+	);
 }
 
-export default Login
+export default Login;
